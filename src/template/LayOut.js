5db99b27442e16a80b97755a1abaab5a86cb3e04
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-07-11 12:46:18 -0500

    new:Sidebar

--- src/template/LayOut.js
+++ src/template/LayOut.js
@@ -3,13 +3,16 @@ import { Container } from './styles'
 import Header from '../componentes/header/Header';
 import Footer from '../componentes/footer/Footer'
 import SideBar from '../componentes/sidebar/SideBar'
+import LinksSidebar from '../componentes/linksSidebar/LinksSidebar';
 
 const LayOut = ({children}) => {
   return (
     <Container>           
       <Header/>
         {children}
-      <SideBar/>
+      <SideBar>
+        <LinksSidebar/>
+      </SideBar>
       <Footer/>
     </Container>
   )
